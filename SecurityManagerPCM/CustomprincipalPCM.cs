8e6cff24296a19dde45a1d372ecd7822fc3ab450
LerDi1121 <35193496+LerDi1121@users.noreply.github.com>
2019-12-22 19:40:43 +0100

    fix some error

--- SecurityManagerPCM/CustomprincipalPCM.cs
+++ SecurityManagerPCM/CustomprincipalPCM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contract;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Claims;
 using System.Linq;
@@ -6,27 +7,51 @@ using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 using System.ServiceModel;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SecurityManagerPCM
 {
     public class CustomPrincipalPCM : IPrincipal
     {
-        public IIdentity identity = null;
-        public CustomPrincipalPCM(IIdentity windowsIdentity)
+
+        WindowsIdentity identity = null;
+        public CustomPrincipalPCM(WindowsIdentity windowsIdentity)
         {
             identity = windowsIdentity;
         }
+
         public IIdentity Identity
         {
             get { return identity; }
         }
-        
 
-        public bool IsInRole(string role)
+
+        public bool IsInRole(string role)// poziva se samo za win autentifikaciju
         {
-            return true;
-   
+            string userName = Formatter.ParseName(Thread.CurrentPrincipal.Identity.Name);
+            string svrName = Formatter.ParseName( System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+            foreach (IdentityReference group in this.identity.Groups)
+            {
+                SecurityIdentifier sid = (SecurityIdentifier)group.Translate(typeof(SecurityIdentifier));
+                var name = sid.Translate(typeof(NTAccount));
+                string groupName = Formatter.ParseName(name.ToString());
+                string[] permissions;
+                if (RoleConfigPCM.GetPermissions(groupName, out permissions))
+                {
+                    foreach (string permision in permissions)
+                    {
+                        if (permision.Equals(role))
+                        {
+                            Logger.AuthorizationSuccess(userName, svrName);
+                            return true;
+                        }
+                    }
+                }
+            }
+            Logger.AuthorizationFailed(userName, svrName, "The client does not have a permit");
+            return false;
+
         }
     }
 }
